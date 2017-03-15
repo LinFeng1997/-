6de60a977e7846dfe02ed55fc406154361799f16
xuyunfeng <ynublow@gmail.com>
2017-03-15 13:47:38 +0800

    3-15小改动

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { ModalController, LoadingController, ToastController} from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { AbstractComponent } from "../../interfaces/abstract-component";
+import { AppConfig } from '../../app/app.config';
 
-import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+	selector: 'page-about',
+	templateUrl: 'about.html'
 })
-export class AboutPage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+export class AboutPage extends AbstractComponent implements OnInit{
+	howSearch: string = '模糊搜索';
+	constructor(public navCtrl: NavController,
+		public modalCtrl: ModalController,
+		protected loadingCtrl: LoadingController,
+		protected toastCtrl: ToastController,
+		protected cfg: AppConfig
+	) {
+		super(cfg, navCtrl, toastCtrl, loadingCtrl);
+	}
+	ngOnInit() {
+		console.log('Hello');
+	}
 }
